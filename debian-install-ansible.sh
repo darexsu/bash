d5780bc83c5a36e7373815c16fea5f212b9151c0
Darexsu <95475095+darexsu@users.noreply.github.com>
2021-12-19 12:33:55 +0500

    Add files via upload

---
+++ debian-install-ansible.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#Версия debian: 8 - trusty, 9 - xenial, 10 - bionic, 11 - focal.
+	version=focal
+#-----------------------------------------
+
+#Функция статуса выполнения комманд OK/Fail
+
+		function StartScript {
+			echo -n "${blue}[${Commandname}: Start]"	
+			echo -n "${reset}"
+			echo
+		}
+		function EndScript {
+			if [ $? -eq 0 ]; then
+				echo -n "${green}[${Commandname}: OK]"
+			else
+				echo -n "${red}[${Commandname}: Fail]"
+			fi
+			echo -n "${reset}"
+			echo
+		}
+
+		red=$(tput setf 4); green=$(tput setf 2); blue=$(tput setf 3); reset=$(tput sgr0)
+
+#-----------------------------------------
+
+
+	
+	Commandname="Добавление deb в Sources.list.d"
+	StartScript
+	echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu ${version} main">/etc/apt/sources.list.d/ansible.list
+	EndScript
+	
+	Commandname="Установка ключей Ansible"
+	StartScript
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+	EndScript
+	
+	Commandname="Apt update: Обновить базы данных пакетов"
+	StartScript
+	apt update
+	EndScript
+	
+	Commandname="Устанока Ansible"
+	StartScript
+	apt install -y ansible
+	EndScript
+	
+	Commandname="Устанока sshpass"
+	StartScript
+	apt install -y sshpass
+	EndScript
+	
+	Commandname="Проверка Ansible"
+	StartScript
+	ansible --version
+	EndScript
+
